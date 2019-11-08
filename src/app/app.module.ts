251d40ca800858b38ceb678008ae108a3f97fea4
Leandro Silva <leandronstrab@gmail.com>
2019-11-08 11:06:41 -0300

    Aplicando Locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt, 'pt');
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExemploPipesComponent } from './exemplo-pipes/exemplo-pipes.component';
+import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExemploPipesComponent,
+    CamelCasePipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    /*{
+      provide:LOCALE_ID,
+      useValue:'pt'
+    }*/
+    SettingsService,
+    {
+      provide:LOCALE_ID,
+      deps:[SettingsService],
+      useFactory:(SettingsService) => SettingsService.getLocale()
+    }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/exemplo-pipes/exemplo-pipes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplo-pipes',
+  templateUrl: './exemplo-pipes.component.html',
+  styleUrls: ['./exemplo-pipes.component.scss']
+})
+export class ExemploPipesComponent implements OnInit {
+
+  livro:any={
+    titulo:'JavaScript para iniciantes',
+    rating:4.54321,
+    numeroPaginas:314,
+    preco:44.99,
+    dataLancamento: new Date(2016,11,23),
+    url:'www.google.com.br'
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
